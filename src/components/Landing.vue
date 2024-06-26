8792fb3b8ac6e239a74c920fd3f726abf8f647e0
Name <anushqakandan03@gmail.com>
2024-06-26 16:20:07 +0200

    Landing Section

---
+++ src/components/Landing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+     <div class="col">
+        <img src="https://anushqakandan.github.io/All-images/Images/AnushiPro.JPG " alt="profile" class="img-fluid" loading="lazy">
+     </div>
+
+     <div class="col"> 
+        <div id="details">
+           <h1 class="display-1">Anushqa Kandan</h1>
+           <span> {{ jobTitle }}</span>
+        </div> 
+     </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import{ computed, onMounted } from 'vue'
+const jobTitle = 
+computed(() => this.$store.state.jobTtle)
+onMounted(() => { this.$store.dispatch('fetchJobTitle')
+
+})
+
+</script>
+
+<style>
+
+</style>
