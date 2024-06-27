9188e3720cd833f2ffcb1f211ccde10454e536da
Name <anushqakandan03@gmail.com>
2024-06-27 12:29:36 +0200

    HomePage Styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg sticky-top">
     <div class="container-fluid">
       <router-link class="navbar-brand" to="/">
         <img
           src="https://anushqakandan.github.io/All-images/Images/AK logo.jpg"
           alt="logo"
-          class="img-fluid"
+          class="img-thumbnail"
           loading="lazy"/>
       </router-link>
 
@@ -23,7 +23,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            <router-link class="nav-link" to="/">Home</router-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#about">About</a>
